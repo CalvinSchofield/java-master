d2a59f499f90bc75e808f56fe035dda369e656b8
Calvin Schofield <CalvinJSchofield@gmail.com>
2015-11-20 13:45:28 -0600

    Added Max/Min functions for double and array data types

--- ArrayMaster.java
+++ ArrayMaster.java
@@ -2,8 +2,9 @@
 
 public class ArrayMaster {
 
-
+	//====================================================================
 	//Function : Find max int in array of int
+	//====================================================================
 	public int maxInt(int[] array) {
 
 		int max = array[0];
@@ -23,7 +24,9 @@ public class ArrayMaster {
 	}
 
 
+	//====================================================================
 	//Function : find min int in array of int
+	//====================================================================
 	public int minInt(int[] array) {
 
 		int min = array[0];
@@ -43,8 +46,10 @@ public class ArrayMaster {
 	}
 
 
+	//====================================================================
 	//Function : Find max double in array of double
-	public double maxInt(double[] array) {
+	//====================================================================
+	public double maxDouble(double[] array) {
 
 		double max = array[0];
 
@@ -63,14 +68,104 @@ public class ArrayMaster {
 	}
 
 
+	//====================================================================
 	//Function : find min double in array of double
-	public double minInt(double[] array) {
+	//====================================================================
+	public double minDouble(double[] array) {
 
 		double min = array[0];
 
 		for(int index = 1; index < array.length; index++) {
 
 			if(min > array[index]) {
+
+				min = array[index];
+
+			}
+
+		}
+
+		return min;
+
+	}
+
+
+	//====================================================================
+	//Function : Find max float in array of float
+	//====================================================================
+	public float maxFloat(float[] array) {
+
+		float max = array[0];
+
+		for(int index = 1; index < array.length; index++) {
+
+			if(max < array[index]) {
+
+				max = array[index];
+
+			}
+
+		}
+
+		return max;
+
+	}
+
+
+	//====================================================================
+	//Function : find min float in array of float
+	//====================================================================
+	public float minFloat(float[] array) {
+
+		float min = array[0];
+
+		for(int index = 1; index < array.length; index++) {
+
+			if(min > array[index]) {
+
+				min = array[index];
+
+			}
+
+		}
+
+		return min;
+
+	}
+
+
+	//====================================================================
+	//Function : Find longest word in array of String
+	//====================================================================
+	public String maxString(String[] array) {
+
+		String max = array[0];
+
+		for(int index = 1; index < array.length; index++) {
+
+			if(max < array[index].length()) {
+
+				max = array[index];
+
+			}
+
+		}
+
+		return max;
+
+	}
+
+
+	//====================================================================
+	//Function : find shortest word in array of String
+	//====================================================================
+	public String minString(String[] array) {
+
+		String min = array[0];
+
+		for(int index = 1; index < array.length; index++) {
+
+			if(min > array[index].length()) {
 
 				min = array[index];
 
