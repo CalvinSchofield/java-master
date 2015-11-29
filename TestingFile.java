c490614fb2ae96e3cb57ba46e3fbe05d1a73c13d
Calvin Schofield <CalvinJSchoield@gmail.com>
2015-11-28 18:28:30 -0600

    Added basic intArray and instance of ArrayMaster class to use the public functions

--- TestingFile.java
+++ TestingFile.java
@@ -6,7 +6,11 @@ public class Testing {
 
 	public static void main(String[] args) {
 
-		
+		ArrayMaster arrayMaster = new ArrayMaster();
+
+		int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		System.out.println(arrayMaster.maxInt(intArray));
 
 	}	
 
